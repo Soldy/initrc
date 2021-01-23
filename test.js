02cbf8f8ac78b17b421562ee2d2f85e290472957
Soldy <4786022+Soldy@users.noreply.github.com>
2021-01-23 11:18:17 +0000

    eslint
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const initrc = new (require('./index.js').init)();
 
 initrc.start.add(function(){
-    console.log('start')
-},1,"test")
+    console.log('start');
+},1,'test');
 initrc.start.run();
 initrc.stop.add(function(){
-    console.log('stop')
-},1,"test")
+    console.log('stop');
+},1,'test');
 initrc.stop.run();
 
 
